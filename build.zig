08c8744a3ba8f642b3635c1732e13a486e9a7426
BrookJeynes <jeynesbrook@gmail.com>
2024-12-02 13:46:28 +1000

    refactor: simplify `build.zig` by extracting exe creation.

--- build.zig
+++ build.zig
@@ -1,42 +1,44 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-/// Must match the `minimum_zig_version` in `build.zig.zon`.
-const minimum_zig_version = "0.13.0";
-/// Must match the `version` in `build.zig.zon`.
-const version = std.SemanticVersion{ .major = 0, .minor = 5, .patch = 1 };
+const targets: []const std.Target.Query = &.{
+    .{ .cpu_arch = .aarch64, .os_tag = .macos },
+    .{ .cpu_arch = .aarch64, .os_tag = .linux },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux },
+    .{ .cpu_arch = .x86_64, .os_tag = .macos },
+};
 
-pub fn build(b: *std.Build) !void {
-    const build_options = b.addOptions();
-    build_options.step.name = "build options";
-    const build_options_module = build_options.createModule();
-    build_options.addOption([]const u8, "minimum_zig_string", minimum_zig_version);
-    build_options.addOption(std.SemanticVersion, "zfe_version", version);
-
-    // Building targets for release.
-    const build_all = b.option(bool, "build-all-targets", "Build all targets in ReleaseSafe mode.") orelse false;
-    if (build_all) {
-        try build_targets(b, build_options_module);
-        return;
-    }
-
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
+fn createExe(b: *std.Build, exe_name: []const u8, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) !*std.Build.Step.Compile {
     const libvaxis = b.dependency("vaxis", .{ .target = target }).module("vaxis");
     const fuzzig = b.dependency("fuzzig", .{ .target = target }).module("fuzzig");
     const zuid = b.dependency("zuid", .{ .target = target }).module("zuid");
 
     const exe = b.addExecutable(.{
-        .name = "zfe",
+        .name = exe_name,
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
+
     exe.root_module.addImport("vaxis", libvaxis);
     exe.root_module.addImport("fuzzig", fuzzig);
     exe.root_module.addImport("zuid", zuid);
-    exe.root_module.addImport("options", build_options_module);
+
+    return exe;
+}
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // Building targets for release.
+    const build_all = b.option(bool, "all-targets", "Build all targets in ReleaseSafe mode.") orelse false;
+    if (build_all) {
+        try build_targets(b);
+        return;
+    }
+
+    const exe = try createExe(b, "zfe", target, optimize);
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
@@ -48,30 +50,11 @@ pub fn build(b: *std.Build) !void {
     run_step.dependOn(&run_cmd.step);
 }
 
-fn build_targets(b: *std.Build, build_options_module: *std.Build.Module) !void {
-    const targets: []const std.Target.Query = &.{
-        .{ .cpu_arch = .aarch64, .os_tag = .macos },
-        .{ .cpu_arch = .aarch64, .os_tag = .linux },
-        .{ .cpu_arch = .x86_64, .os_tag = .linux },
-        .{ .cpu_arch = .x86_64, .os_tag = .macos },
-    };
-
+fn build_targets(b: *std.Build) !void {
     for (targets) |t| {
         const target = b.resolveTargetQuery(t);
-        const libvaxis = b.dependency("vaxis", .{ .target = target }).module("vaxis");
-        const fuzzig = b.dependency("fuzzig", .{ .target = target }).module("fuzzig");
-        const zuid = b.dependency("zuid", .{ .target = target }).module("zuid");
 
-        const exe = b.addExecutable(.{
-            .name = "zfe",
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = .ReleaseSafe,
-        });
-        exe.root_module.addImport("fuzzig", fuzzig);
-        exe.root_module.addImport("vaxis", libvaxis);
-        exe.root_module.addImport("zuid", zuid);
-        exe.root_module.addImport("options", build_options_module);
+        const exe = try createExe(b, "zfe", target, .ReleaseSafe);
         b.installArtifact(exe);
 
         const target_output = b.addInstallArtifact(exe, .{
