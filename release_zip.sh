21643afc3e147bcf93f1a4b30373127424c0098d
BrookJeynes <jeynesbrook@gmail.com>
2024-12-11 15:02:14 +1000

    build: no longer needed due to actions

--- release_zip.sh
+++
@@ -1,6 +0,0 @@
-cd ./zig-out
-zip -r ./x86_64-linux.zip ./x86_64-linux/
-zip -r ./x86_64-macos.zip ./x86_64-macos/
-zip -r ./aarch64-linux.zip ./aarch64-linux/
-zip -r ./aarch64-macos.zip ./aarch64-macos/
-cd -
